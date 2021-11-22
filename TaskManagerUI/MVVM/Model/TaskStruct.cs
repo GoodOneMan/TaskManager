cf7f611436c3d7fa27383dc12f3da6ce939f39ca
GoodOneMan <myhut@rambler.ru>
2021-11-22 08:12:23 +0300

    Task manager project commit github

---
+++ TaskManagerUI/MVVM/Model/TaskStruct.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagerUI.Core;
+
+namespace TaskManagerUI.MVVM.Model
+{
+    class TaskStruct : BaseViewModel
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        private string _user;
+        public string User
+        {
+            get { return _user; }
+            set
+            {
+                _user = value;
+                OnPropertyChanged("User");
+            }
+        }
+
+        private bool _isChecked;
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+            set
+            {
+                _isChecked = value;
+                OnPropertyChanged("IsChecked");
+            }
+        }
+
+        private bool _state;
+        public bool State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                OnPropertyChanged("State");
+            }
+        }
+
+        private string _comment;
+        public string Comment
+        {
+            get { return _comment; }
+            set
+            {
+                _comment = value;
+                OnPropertyChanged("Comment");
+            }
+        }
+    }
+}
---
+++ TaskManagerUI/MVVM/ViewModel/MainWindowModel.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TaskManagerUI.Core;
+using TaskManagerUI.MVVM.Model;
+
+namespace TaskManagerUI.MVVM.ViewModel
+{
+    class MainWindowModel : BaseViewModel
+    {
+
+        #region Test data
+        public MainWindowModel()
+        {
+
+            Tasks = new List<TaskStruct>();
+
+            for (int i = 0; i < 2000; i++)
+            {
+                TaskStruct t = new TaskStruct
+                {
+                    Title = "Task " + i.ToString(),
+                    Description = "Description " + i.ToString(),
+                    IsChecked = false,
+                    State = false,
+                    User = ""
+                };
+
+                Tasks.Add(t);
+            }
+        }
+
+        //public ObservableCollection<TaskStruct> _tasks;
+        //public ObservableCollection<TaskStruct> Tasks
+        //{
+        //    get { return _tasks; }
+        //    set
+        //    {
+        //        _tasks = value;
+        //        OnPropertyChanged("Tasks");
+        //    }
+        //}
+
+        public List<TaskStruct> _tasks;
+        public List<TaskStruct> Tasks
+        {
+            get { return _tasks; }
+            set
+            {
+                _tasks = value;
+                OnPropertyChanged("Tasks");
+            }
+        }
+
+        private RelayCommand _isCheckedModel;
+        public RelayCommand IsCheckedCommand
+        {
+            get
+            {
+                return _isCheckedModel ?? (_isCheckedModel = new RelayCommand(
+                    obj =>
+                    {
+                        TaskStruct task = Tasks.FirstOrDefault(item => item == (TaskStruct)obj);
+
+                        if(task != null)
+                        {
+                            if(task.IsChecked)
+                                task.User = Environment.UserName;
+                            else
+                                task.User = "";
+                        }
+
+                        List<TaskStruct> temp = new List<TaskStruct>();
+                        foreach(TaskStruct t in Tasks)
+                        {
+                            if (t.IsChecked)
+                                temp.Add(t);
+                        }
+                        foreach (TaskStruct t in Tasks)
+                        {
+                            if (!t.IsChecked)
+                                temp.Add(t);
+                        }
+                        Tasks.Clear();
+                        Tasks = temp;
+
+                        //Tasks.OrderBy(item => item.IsChecked ? 0 : 1);
+                        //List<TaskStruct> tmpTasks = new List<TaskStruct>();
+                        //foreach(TaskStruct t in Tasks)
+                        //{
+                        //    if (t.IsChecked)
+                        //        tmpTasks.Add(t);
+                        //}
+
+                        //foreach (TaskStruct t in Tasks)
+                        //{
+                        //    if (!t.IsChecked)
+                        //        tmpTasks.Add(t);
+                        //}
+
+                        //Tasks.Clear();
+                        //foreach (TaskStruct t in tmpTasks)
+                        //{
+                        //    Tasks.Add(t);
+                        //}
+
+                        //ObservableCollection<TaskStruct> temp;
+                        //temp = new ObservableCollection<TaskStruct>(Tasks.OrderBy(item => item.IsChecked));
+                        //Tasks = temp;
+
+                        //CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(Tasks);
+                        //view.SortDescriptions.Add(new SortDescription("Age", ListSortDirection.Ascending));
+
+                    }));
+            }
+        }
+
+        private RelayCommand _setComment;
+        public RelayCommand SetComment
+        {
+            get
+            {
+                return _setComment ?? (_setComment = new RelayCommand(
+                    obj =>
+                    {
+                        
+                    }));
+            }
+        }
+
+        private RelayCommand _closeTask;
+        public RelayCommand CloseTask
+        {
+            get
+            {
+                return _closeTask ?? (_closeTask = new RelayCommand(
+                    obj =>
+                    {
+
+
+                    }));
+            }
+        }
+        #endregion
+
+    }
+}
