e7352e026930e1b3a8e0ad7ae850be576fe1deee
GoodMan <GoodMan@GOODMANHOST>
2021-11-22 21:14:54 +0300

    Add repsitory and ...

---
+++ TaskManagerUI/MVVM/Model/Repository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TaskManagerUI.Core;
+
+namespace TaskManagerUI.MVVM.Model
+{
+    class Repository : BaseViewModel
+    {
+        private static Repository instance;
+
+        public static Repository GetRepository()
+        {
+            if (instance == null)
+                instance = new Repository();
+
+            return instance;
+        }
+
+        private Repository()
+        {
+            Tasks = new ObservableCollection<TaskStruct>();
+            SelectedTask = null;
+            CommentWindow = null;
+        }
+
+        public ObservableCollection<TaskStruct> Tasks;
+        public TaskStruct SelectedTask;
+        public Window CommentWindow;
+    }
+}
